dad444314658a20e98e15fe4b5333fd8eee4b7d0
lytvyn139 <lytvyn.139@gmail.com>
2020-09-27 22:26:58 -0500

    added sign in

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from "react";
 import { render } from "react-dom";
 import App from "./app";
 import { GlobalStyles } from "./global-styles";
+import { firebase } from "./lib/firebase.prod.js";
+import { FirebaseContext } from "./context/firebase";
 
 render(
     <>
-        ,
-        <GlobalStyles />
-        <App />
+        <FirebaseContext.Provider value={{ firebase }}>
+            <GlobalStyles />
+            <App />
+        </FirebaseContext.Provider>
     </>,
     document.getElementById("root")
 );
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Signup() {
-    return <p> Hello from sign up </p>;
+    return <p> SignupSignupSignupSignupSignupSignup </p>;
 }
