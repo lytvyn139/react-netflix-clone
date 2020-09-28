c5c3eca7b5a58161588300a356ed74f2cc2294b7
lytvyn139 <lytvyn.139@gmail.com>
2020-09-28 14:23:19 -0500

    added browse

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -45,9 +45,40 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
     );
 };
 
-// Header.Group = function HeaderGroup({ children, ...restProps }) {
-//     return <Group {...restProps}>{children}</Group>;
-//   };
+Header.Feature = function HeaderFeature({ children, ...restProps }) {
+    return <Feature {...restProps}>{children}</Feature>;
+};
+
+Header.Text = function HeaderText({ children, ...restProps }) {
+    return <Text {...restProps}>{children}</Text>;
+};
+
+Header.FeatureCallOut = function HeaderFeatureCallOut({
+    children,
+    ...restProps
+}) {
+    return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
+};
+
+Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
+    return <Link {...restProps}>{children}</Link>;
+};
+
+Header.Group = function HeaderGroup({ children, ...restProps }) {
+    return <Group {...restProps}>{children}</Group>;
+};
+
+Header.Profile = function HeaderProfile({ children, ...restProps }) {
+    return <Profile {...restProps}>{children}</Profile>;
+};
+
+Header.Picture = function HeaderPicture({ src, ...restProps }) {
+    return <Picture {...restProps} src={`/images/users/${src}.png`} />;
+};
+
+Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
+    return <Dropdown {...restProps}>{children}</Dropdown>;
+};
 
 // Header.Search = function HeaderSearch({ searchTerm, setSearchTerm, ...restProps }) {
 //   const [searchActive, setSearchActive] = useState(false);
@@ -68,34 +99,10 @@ Header.Logo = function HeaderLogo({ to, ...restProps }) {
 //   );
 // };
 
-// Header.Profile = function HeaderProfile({ children, ...restProps }) {
-//   return <Profile {...restProps}>{children}</Profile>;
-// };
-
 // Header.Feature = function HeaderFeature({ children, ...restProps }) {
 //   return <Feature>{children}</Feature>;
 // };
 
-// Header.Picture = function HeaderPicture({ src, ...restProps }) {
-//   return <Picture {...restProps} src={`/images/users/${src}.png`} />;
-// };
-
-// Header.Dropdown = function HeaderDropdown({ children, ...restProps }) {
-//   return <Dropdown {...restProps}>{children}</Dropdown>;
-// };
-
-// Header.TextLink = function HeaderTextLink({ children, ...restProps }) {
-//   return <Link {...restProps}>{children}</Link>;
-// };
-
 // Header.PlayButton = function HeaderPlayButton({ children, ...restProps }) {
 //   return <PlayButton {...restProps}>{children}</PlayButton>;
-// };
-
-// Header.FeatureCallOut = function HeaderFeatureCallOut({ children, ...restProps }) {
-//   return <FeatureCallOut {...restProps}>{children}</FeatureCallOut>;
-// };
-
-// Header.Text = function HeaderText({ children, ...restProps }) {
-//   return <Text {...restProps}>{children}</Text>;
 // };
